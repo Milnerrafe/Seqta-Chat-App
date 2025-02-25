55b955d75d88b678457ba4914a77fe1d6ebba5f2
Rafe Milner <milnerrafe@outlook.com>
2025-02-25 20:14:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, send_from_directory, file
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory
 from flask_socketio import SocketIO, send, join_room, leave_room  # Import the necessary functions
 import subprocess
 import random
@@ -20,10 +20,6 @@ def index():
 @app.route('/index.json')
 def get_json():
     return send_from_directory('templates', 'index.json')
-
-@app.route('/favicon.svg')
-def get_favicon():
-    return file('templates/favicon.svg').read()
 
 @app.route('/create_room', methods=['POST'])
 def create_room():
